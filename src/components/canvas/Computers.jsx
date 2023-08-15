9351947f7fcfef7dcdb87e63e6a6d534f8e67dfe
Anmol99535 <anmol.garg594@gmail.com>
2023-08-15 12:55:10 +0530

    3rd Commit

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -58,7 +58,7 @@ const ComputersCanvas = () => {
       frameloop='demand'
       shadows
       dpr={[1, 2]}
-      camera={{ position: [20, 3, 5], fov: 25 }}
+      camera={{ position: [20, 3, 5], fov: 45 }}
       gl={{ preserveDrawingBuffer: true }}
     >
       <Suspense fallback={<CanvasLoader />}>
