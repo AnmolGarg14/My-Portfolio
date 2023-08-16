3f5ac9ce231274816ed1104a2d74aec2252bac2c
Anmol Garg <113035539+AnmolGarg14@users.noreply.github.com>
2023-08-16 21:04:07 +0530

    Final Commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -19,8 +19,7 @@ const Hero = () => {
             Hi, I'm <span className="text-[#915EFF]">Anmol</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I develop user interfaces and
-            <br className="sm:block hidden" />
+            I develop user interfaces and <br className="sm:block hidden" />
             web applications
           </p>
         </div>
