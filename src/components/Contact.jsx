7f302461c7e0e4ca58b92886ff318c18d966abf5
Anmol99535 <anmol.garg594@gmail.com>
2023-08-16 13:49:57 +0530

    3rd Commit

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -79,7 +79,7 @@ const Contact = () => {
         <form
           ref={formRef}
           onSubmit={handleSubmit}
-          className='mt-12 flex flex-col gap-8'
+          className='mt-12 flex flex-col gap-8 max-xs:mb-6'
         >
           <label className='flex flex-col'>
             <span className='text-white font-medium mb-4'>Your Name</span>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,6 @@ import { motion } from "framer-motion";
 
 import { styles } from "../styles";
 import { ComputersCanvas } from "./canvas";
-import { EarthCanvas } from "./canvas";
 
 const Hero = () => {
   return (
@@ -26,14 +25,7 @@ const Hero = () => {
         </div>
       </div>
 
-      {/* <ComputersCanvas />*/}
-
-      <motion.div
-        // variants={slideIn("right", "tween", 0.2, 1)}
-        className='xl:flex-1 xl:h-auto md:h-[550px] h-[350px]'
-      >
-        <EarthCanvas />
-      </motion.div>
+      <ComputersCanvas />
 
       <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
         <a href='#about'>
