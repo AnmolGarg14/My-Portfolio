6e2bed18d04bb0246ba4851a2662cfeaab349a08
Anmol Garg <anmol.garg594@gmail.com>
2023-12-22 14:15:36 +0530

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -14,32 +14,23 @@ import {
 
 const App = () => {
   return (
-    <>
-      <head>
-        <script
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1881486874855700"
-          crossorigin="anonymous"
-        ></script>
-      </head>
-      <BrowserRouter>
-        <div className="relative z-0 bg-primary">
-          <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
-            <Navbar />
-            <Hero />
-          </div>
-          <About />
-          <Experience />
-          <Tech />
-          <Works />
-          {/* <Feedbacks /> */}
-          <div className="relative z-0">
-            <Contact />
-            <StarsCanvas />
-          </div>
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar />
+          <Hero />
         </div>
-      </BrowserRouter>
-    </>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        {/* <Feedbacks /> */}
+        <div className="relative z-0">
+          <Contact />
+          <StarsCanvas />
+        </div>
+      </div>
+    </BrowserRouter>
   );
 };
 
